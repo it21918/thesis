d1e613b9e90bab52c8b45a5170d804fa1dc2a292
it21918 <79996442+it21918@users.noreply.github.com>
2023-03-02 21:03:09 +0200

    Add files via upload

--- medicalApp/settings.py
+++ medicalApp/settings.py
@@ -122,6 +122,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 AUTH_USER_MODEL="base.CustomUser"
 AUTHENTICATION_BACKENDS=['base.emailBackEnd.EmailBackEnd']
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- medicalApp/urls.py
+++ medicalApp/urls.py
@@ -15,13 +15,37 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base import views
+from base import views, doctorViews, adminViews
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('data', adminViews.pivot_data, name='pivot_data'),
     path('', views.showLoginPage, name="showLoginPage"),
     path('doLogin', views.doLogin, name="doLogin"),
     path('logout', views.logout, name="logout"),
     path('sign_up', views.showSignUpPage, name="signUp"),
-    path('doSignUp', views.doSignUp, name='doSignUp')
+    path('doSignUp', views.doSignUp, name='doSignUp'),
+    path('doctorHome', doctorViews.doctorHome, name="doctorHome"),
+    path('adminHome', adminViews.adminHome, name="adminHome"),
+    path('modifyImages', adminViews.modifyImages, name="modifyImages"),
+    path('evaluateModel', adminViews.evaluateModel, name="evaluateModel"),
+    path('evaluation', adminViews.evaluation, name="evaluation"),
+    path('modifyDoctors', adminViews.modifyDoctors, name="modifyDoctors"),
+    path('add_image', adminViews.addImage, name="add_image"),
+    path('Segmentation', doctorViews.segmentation, name="segmentation"),    
+    path('Patients', doctorViews.patients, name="patients"),    
+    path('delete_image/<str:image_id>', adminViews.deleteImage,name="delete_image"),
+    path('delete_doctor/<str:doctor_id>', adminViews.deleteDoctor,name="delete_doctor"),
+    path('editDoctor/<str:doctor_id>', adminViews.editDoctor,name="editDoctor"),
+    path('edit_doctor_save', adminViews.editDoctorSave,name="edit_doctor_save"),
+    path('trainModel', adminViews.train, name='train'),
+    path('train_selected', adminViews.trainSelected, name='train_selected'),
+    path('train_results', adminViews.train_results, name='train_results'),
+    path('run_detail/<str:run_id>/', adminViews.run_detail, name='run_detail'),
+
 ]
+ 
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
